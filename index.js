b3c6e644be7170deda1734d01be3379bc397b801
mahtabesmaeili1 <mahtabesmaeilii13@gmail.com>
2023-04-12 17:02:15 +0200

    let const and values

--- index.js
+++ index.js
@@ -1,2 +1,20 @@
 //comment
 console.log("Hello World!!");
+//node index.js in the terminal also execute the code in the terminal
+
+let firstName = "Mahtab";
+console.log(firstName);
+
+let lastName = "Esmaeili";
+let interestRate = 0.3;
+interestRate = 1;
+console.log(interestRate);
+
+const mahtab = "owner";
+// mahtab = "sina"; we can not resign the constant
+let age = 28;
+let isApproved = false;
+let last = null;
+let selectedColor = null;
+selectedColor = "green";
+//primitives or value
