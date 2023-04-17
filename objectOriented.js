8da639912aefd55172e74dac129583fab6555b7c
mahtabesmaeili1 <mahtabesmaeilii13@gmail.com>
2023-04-17 20:32:41 +0200

    object oriented programming explanation

--- objectOriented.js
+++ objectOriented.js
@@ -1,5 +1,5 @@
 //object oriented programming OOP
-
+//1-Encapsulation
 let baseSalary = 30_000;
 let overTime = 10;
 let rate = 20;
@@ -7,7 +7,7 @@ function getWage(baseSalary, overTime, rate) {
   return baseSalary + overTime * rate;
 }
 // up here is a normal way
-//down there is how object oriented programing works, its like a unite.
+//down there is how object oriented programing works, its like a unite and has no params , so its best and easy to use and maintain and reduce the compexity and increase reusability.
 let employee = {
   baseSalary: 30_000,
   overTime: 10,
@@ -16,3 +16,8 @@ let employee = {
     return this.baseSalary + this.overTime * this.rate;
   },
 };
+
+//2-abstraction (hide the details and complexity and when it change doesnt effect much in the whole , simpler interface / reduce the impact of the change.)
+
+//3-inheritance (eliminate redundant code (in yani code haye ezafi ra hazf konid))
+//4-poly morphism (means many form  ( refactor ugly switch / case statement))
