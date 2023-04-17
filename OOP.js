6a589609f8133dbee2d0149239676bf0852cb42e
mahtabesmaeili1 <mahtabesmaeilii13@gmail.com>
2023-04-17 20:44:48 +0200

    method and properties

--- OOP.js
+++ OOP.js
@@ -1,1 +1,17 @@
 console.log("hello world");
+//object
+//creating an object
+//object is a collection of key value pairs
+const circle = {
+  radius: 1,
+  location: {
+    x: 1,
+    y: 1,
+  },
+  //draw is a method which holds some logic others are properties which holds values
+  draw: function () {
+    console.log("draw");
+  },
+};
+//using dot notation to access
+circle.draw();
