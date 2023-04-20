e5709433b3b3763a56255d78f88adaffc1825c40
mahtabesmaeili1 <mahtabesmaeilii13@gmail.com>
2023-04-20 20:14:26 +0200

    array

---
+++ array.js
@@ -0,0 +1,8 @@
+const selectedColors = ["red", "blue"];
+selectedColors[2] = "green";
+selectedColors[3] = 1;
+console.log(selectedColors[0]);
+console.log(selectedColors);
+//node array.js
+//an array is an object type
+console.log(selectedColors.length);
