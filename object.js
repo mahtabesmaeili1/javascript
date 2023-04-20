97c4e72423effd17b64ce3bb3dc537a66a8ce7e8
mahtabesmaeili1 <mahtabesmaeilii13@gmail.com>
2023-04-20 20:31:27 +0200

    array

---
+++ object.js
@@ -0,0 +1,25 @@
+const circle = {
+  radius: 1,
+  draw() {
+    console.log("draw");
+  },
+};
+
+//this is a old way to do this
+// const another = {};
+// for (let key in circle) another[key] = circle[key];
+
+//this is also another way to clone an object but not the best
+
+// const another = Object.assign(
+//   {
+//     color: "yellow",
+//   },
+//   circle
+// );
+// console.log(another);
+
+//spread operator
+// const another = { ...circle };
+const another = { ...circle, color: "pink" };
+console.log(another);
